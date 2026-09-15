609c0824c071edd529b48db5f242128ade2d9d08
Efehan Mert <mertefehan2010@gmail.com>
2026-09-15 20:59:49 +0300

    Complete app implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,8 +38,51 @@ class BenimUygulamam extends StatelessWidget {
   }
 }
 
-class YemekSayfasi extends StatelessWidget {
-  const YemekSayfasi({super.key});
+class YemekSayfasi extends StatefulWidget {
+  YemekSayfasi({super.key});
+
+  @override
+  State<YemekSayfasi> createState() => _YemekSayfasiState();
+}
+
+class _YemekSayfasiState extends State<YemekSayfasi> {
+  int corbaNo = 1;
+  int yemekNo = 1;
+  int tatliNo = 1;
+
+  List<String> corbaAdlari = [
+    'Mercimek Çorbası',
+    'Tarhana Çorbası',
+    'Tavuksuyu Çorbası',
+    'Düğün Çorbası',
+    'Yoğurtlu Çorbası',
+  ];
+
+  List<String> yemekAdlari = [
+    'Karnıyarık Yemeği',
+    'Mantı',
+    'Kuru Fasulye',
+    'İçli Köfte',
+    'Fırında Balık',
+  ];
+
+  List<String> tatliAdlari = [
+    'Kadayıf Tatlısı',
+    'Cevizli Baklava',
+    'Fırın Sütlaç',
+    'Kazandibi',
+    'Dondurma',
+  ];
+
+  void yemekCesitleriniYenile() {
+    setState(
+      () {
+        corbaNo = Random().nextInt(5) + 1;
+        yemekNo = Random().nextInt(5) + 1;
+        tatliNo = Random().nextInt(5) + 1;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,19 +92,80 @@ class YemekSayfasi extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Image.asset('assets/images/corba_1.jpg'),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  //backgroundColor: Colors.amber,
+                  foregroundColor: Colors.white,
+                  overlayColor: Colors.white.withValues(alpha: 0.3),
+                ),
+                onPressed: yemekCesitleriniYenile,
+                child: Image.asset('assets/images/corba_$corbaNo.jpg'),
+              ),
+            ),
+          ),
+          Text(
+            corbaAdlari[corbaNo - 1],
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            width: 250,
+            child: Divider(
+              height: 5,
+              color: Colors.redAccent,
             ),
           ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Image.asset('assets/images/yemek_1.jpg'),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  overlayColor: Colors.white.withValues(alpha: 0.3),
+                ),
+                onPressed: yemekCesitleriniYenile,
+                child: Image.asset('assets/images/yemek_$yemekNo.jpg'),
+              ),
+            ),
+          ),
+          Text(
+            style: TextStyle(
+              fontSize: 20,
+            ),
+            yemekAdlari[yemekNo - 1],
+          ),
+          SizedBox(
+            width: 250,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
             ),
           ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Image.asset('assets/images/tatli_1.jpg'),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  overlayColor: Colors.white.withValues(alpha: 0.3),
+                ),
+                onPressed: yemekCesitleriniYenile,
+                child: Image.asset('assets/images/tatli_$tatliNo.jpg'),
+              ),
+            ),
+          ),
+          Text(
+            style: TextStyle(
+              fontSize: 20,
+            ),
+            tatliAdlari[tatliNo - 1],
+          ),
+          SizedBox(
+            width: 250,
+            child: Divider(
+              height: 5,
+              color: Colors.greenAccent,
             ),
           ),
         ],
